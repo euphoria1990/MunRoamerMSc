acde94a00a6f0335a1518e94209fb6c6e15b60dc
Kirsty <xkirstyx90@hotmail.com>
2021-07-23 21:34:20 +0100

    Started code in New Account for database.  Have not made database class yet.

--- app/src/main/java/com/assignment/munroamer/New_Account.java
+++ app/src/main/java/com/assignment/munroamer/New_Account.java
@@ -2,14 +2,38 @@ package com.assignment.munroamer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class New_Account extends Activity {
+
+    EditText eT1, eT2, eT3, eT4, eT5;
+    Button sgnUpBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_account);
+        eT1 = (EditText)findViewById(R.id.newFirstName);
+        eT2 = (EditText)findViewById(R.id.newLastName);
+        eT3 = (EditText)findViewById(R.id.newEmail);
+        eT4 = (EditText)findViewById(R.id.newPassword1);
+        eT5 = (EditText)findViewById(R.id.newPassword2);
 
-        this.setTitle("New Account");
+        sgnUpBtn = (Button)findViewById(R.id.signMeUpBtn);
+        sgnUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str1 = eT1.getText().toString();
+                String str2 = eT2.getText().toString();
+                String str3 = eT3.getText().toString();
+                String str4 = eT4.getText().toString();
+                String str5 = eT5.getText().toString();
+            }
+        });
+
+        //this.setTitle("New Account");
     }
 }
 
