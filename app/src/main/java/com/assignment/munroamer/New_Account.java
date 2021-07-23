527c74cb2357de8734338a7ebbc306e78cf568c9
Kirsty <xkirstyx90@hotmail.com>
2021-07-23 20:54:18 +0100

    Login and Sign Up Pages designed.  No functionality yet.

---
+++ app/src/main/java/com/assignment/munroamer/New_Account.java
@@ -0,0 +1,15 @@
+package com.assignment.munroamer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class New_Account extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_account);
+
+        this.setTitle("New Account");
+    }
+}
+
