bf8b298d8b6b82214ccf37a17dbc7d679c48dddb
Kirsty <xkirstyx90@hotmail.com>
2021-07-25 17:08:05 +0100

    Made the Sign Up Here Text Clickable

--- app/src/main/java/com/assignment/munroamer/MainActivity.java
+++ app/src/main/java/com/assignment/munroamer/MainActivity.java
@@ -2,16 +2,19 @@ package com.assignment.munroamer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText eMl1, passW1;
     Button lgn1;
+    TextView sgnUp;
     DatabaseHelper db;
 
     @Override
@@ -23,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         eMl1 = (EditText)findViewById(R.id.emailLogin);
         passW1 = (EditText)findViewById(R.id.loginPasswordText);
         lgn1 = (Button)findViewById(R.id.loginButton);
+
+        sgnUp = (TextView)findViewById(R.id.signUpText);
 
         lgn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +41,14 @@ public class MainActivity extends AppCompatActivity {
                 else {
                     Toast.makeText(getApplicationContext(), "Incorrect email or password.  Please try again", Toast.LENGTH_LONG).show();
                 }
+            }
+        });
+
+        sgnUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,New_Account.class);
+                startActivity(intent);
             }
         });
     }
